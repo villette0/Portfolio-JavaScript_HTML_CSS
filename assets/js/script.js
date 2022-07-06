325d95f087aed70f8628dfeb577b308ed5d6ae7a
violet978 <villette@live.com>
2022-07-06 16:36:43 -0700

    Fixed centering in CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,42 @@
 // Universal variables first
+var projectsBtn = document.querySelector(".projects-button");
+var aboutBtn = document.querySelector(".about-button");
+var contactBtn = document.querySelector(".contact-button");
+var projectsSection = document.querySelector(".projects-section");
+var aboutSection = document.querySelector(".about-section");
+var contactSection = document.querySelector(".contact-section");
 
 
+projectsBtn.addEventListener("click", hideAllButProjects);
+aboutBtn.addEventListener("click", hideAllButAbout);
+contactBtn.addEventListener("click", hideAllButContact);
+
+
+//Initial screenload
+document.addEventListener('DOMContentLoaded', hideContainers);
+
+function hideContainers() {
+    // Initially hide the other containers except projects
+    projectsSection.style.display = 'block';
+    aboutSection.style.display = 'none';
+    contactSection.style.display = 'none';
+}
+
+
+function hideAllButProjects() {
+    projectsSection.style.display = 'block';
+    aboutSection.style.display = 'none';
+    contactSection.style.display = 'none';
+}
+
+function hideAllButAbout() {
+    aboutSection.style.display = 'block';
+    projectsSection.style.display = 'none';
+    contactSection.style.display = 'none';
+}
+
+function hideAllButContact() {
+    contactSection.style.display = 'block';
+    projectsSection.style.display = 'none';
+    aboutSection.style.display = 'none';
+}
